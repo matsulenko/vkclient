2dc70a0e17584a51e3164334a813bb3e5c28aeb9
matsulenko <matsulenko@mail.ru>
2023-12-27 04:12:37 +0300

    Diploma is updated

--- VKClient/Model/Auth/InfoPlstData.swift
+++ VKClient/Model/Auth/InfoPlstData.swift
@@ -12,5 +12,5 @@ enum InfoPlist {
         Bundle.main.infoDictionary?["CLIENT_ID"] as? String
     }
     
-    static var tokenForPreviews: String = "vk1.a.1nDNkAWCp9czwCe-UkgUawc59KnO_hEkHbwcpuNd7aC1Bu_GXMh1wBHnwlfiYnYRA-qFaSiooRMSRHhx1aNAXKa5ZzVrnCa9S4qLwnU8ciE0HiwKjQXtNR5vIY2d-Ur4lAmb5uOqn7KIrRbmoUHzjqTHZYxQoi2dr-_6dWGKJ8AqAZC2eojUu3-TlHO1mbuEp_n7umi0kWWSdhUy_89aiA"
+    static var tokenForPreviews: String = ""
 }
